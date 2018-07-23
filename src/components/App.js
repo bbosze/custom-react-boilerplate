2427c2bc7ee44605982d421b36de0c492c68fba4
bbosze <b.bosze@gmail.com>
2018-07-23 18:33:10 +0200

    test OK

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-// const App = () => {
-//   const title = 'Nagyon Jól Működik';
-//   return (
-//     <div>
-//       {title}
-//     </div>
-//   );
-// };
+const App = () => {
+  const title = 'Nagyon Jól Működik';
+  return (
+    <div>
+      {title}
+    </div>
+  );
+};
 
 export default App;
